022b1c1cfa9da3cb4f55b7ad4b30218840ad077c
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-03-03 09:08:24 +0530

    added
    Components

---
+++ class_based_component/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Component } from "react";
+class Nav extends Component{
+    render(){
+        return <h1>heyyy</h1>
+    }
+}
+export default Nav
---
+++ function_based_component/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Logo=()=>{
+    return(
+        <h1>riya</h1>
+    )
+}
+export default Logo
