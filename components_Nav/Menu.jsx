d90fb7efefe9615de9ab9dc745b1cd645f57e895
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-03-05 18:14:47 +0530

    initial_commit
    Simple Navigation Bar using ReactJS.
    App.jsx (import Nav.jsx)(import global.css)--->Nav.jsx(import Logo.jsx)(import Menu.jsx(home,login,signup,about))

---
+++ components_Nav/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// import Menu from './Menu';
+
+
+const Menu=()=>{
+    return(
+        <header>
+        <nav class="nav_bar">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">ContactUs</a></li>
+             <li><a href="">Services</a></li>
+             {/* <div class="search">
+                <input type="text" name="search" id="search" placeholder="Search ">
+             </div> */}
+            </ul>
+        </nav>
+    </header>
+    )
+}
+export default Menu
