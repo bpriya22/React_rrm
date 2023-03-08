c421b246035f8521fe07008ad2c56314e284293f
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-03-08 18:36:39 +0530

    added
    Props in the function-based component.
    Props in the class-based component.

---
+++ Props/function_based/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Nav=(props)=>{
+    return(
+    <h1>{props.data}</h1>
+    )
+}
+export default Nav
---
+++ Props/function_based/Nav3.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Nav3=(props)=>{
+    return(
+         <h2>{props.data}</h2>
+    )
+}
+export default Nav3
