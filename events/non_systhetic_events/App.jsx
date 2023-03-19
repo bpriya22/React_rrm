f4434d9a3665e6a146d50444d25064b9530540d4
Priyanka B <89992324+bpriya22@users.noreply.github.com>
2023-03-19 13:34:33 +0530

    initial commit

--- events/non_systhetic_events/App.jsx
+++ events/non_systhetic_events/App.jsx
@@ -1,25 +1,52 @@
-import React,{Component} from 'react';
-export default class App extends Component{
+// import React,{Component} from 'react';
+// export default class App extends Component{
+//   constructor()
+//   {
+//     super()
+//     this.state={
+//       username:"UPENDRA"
+//     }
+//   }
+//   componentDidMount()
+//   {
+//     let btn=document.querySelector('button')
+//     btn.addEventListener('click',()=>{
+//       this.setState({username:this.state.username="PRIYANKA"})
+//     })
+
+//   }
+//   render(){
+//     return(
+//       <>
+//       <div>{this.state.username}</div>
+//       <button>Change</button>
+//       </>
+//     )
+//   }
+// }
+import React, { Component } from 'react'  //textbox->type inside to display.
+
+export default class App extends Component {
   constructor()
   {
     super()
     this.state={
-      username:"UPENDRA"
+      username:" "
     }
   }
-  componentDidMount()
-  {
-    let btn=document.querySelector('button')
-    btn.addEventListener('click',()=>{
-      this.setState({username:this.state.username="PRIYANKA"})
-    })
 
+  componentDidMount(){
+   let input=document.querySelector('input')
+   input.addEventListener("keypress",(x)=>{
+     this.setState({username:x.target.value})
+   })
   }
-  render(){
-    return(
+ 
+  render() {
+    return (
       <>
-      <div>{this.state.username}</div>
-      <button>Change</button>
+      <h1>{this.state.username}</h1>
+      <input type="text" />
       </>
     )
   }
